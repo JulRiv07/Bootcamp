b68579990cb639d6b3b7831036371631c5bf91a3
JulRiv07 <Julian.rivera@utp.edu.co>
2025-10-05 00:35:02 -0500

    Empezando Java script

---
+++ JavaScript/PrimerosPasos.js
@@ -0,0 +1,19 @@
+//? Variables
+let edad = 18;
+const PI = 3.1416
+let nombre = "Julian";
+let llueve = false;
+
+//? Mostrar datos en pantalla
+console.log("Hola, mundo Java Script");
+alert("¡Virus!"); //! Ventana emergente
+
+//? Pedir datos al usuario
+const name = prompt("Como es tu nombre? "); //! prompt (Siempre recibe letras)
+alert("Hola, " + name); 
+
+const a = number(prompt("Ingresar un numero: "));
+const b = number(prompt("Ingrese otro numero: "));
+alert("La suma es: " + (a + b));
+
+
